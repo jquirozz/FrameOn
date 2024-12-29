b217fc880623745a225134d3e5b8fff7bb2ec6ee
jquirozz <jq84144@gmail.com>
2024-12-29 18:27:37 -0500

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Frame On</h1>
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
